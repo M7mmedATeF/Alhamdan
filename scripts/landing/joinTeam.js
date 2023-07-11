04b2c77ae29bfdda26c76021aec753b4094ce71a
Mohammed Atef <mohammed.atef.ewais@gmail.com>
2023-07-11 13:00:53 +0300

    finishing landpage

--- scripts/landing/joinTeam.js
+++ scripts/landing/joinTeam.js
@@ -6,7 +6,34 @@ function hide(){
     form.classList.toggle('hidden')
 }
 
-console.log(form);
-
 overlay.addEventListener('click', hide)
 BTN.addEventListener('click', hide)
+
+// adding file input
+let cvCTRL = document.querySelector('.cvForm .formControl.file')
+let cvPrev = document.querySelector('#cvPrev')
+let cvINP = document.querySelector('.cvForm .cvINP')
+cvINP.addEventListener('change', setFile)
+
+function setFile(e){
+    let file = e.target.files[0]
+    console.log(file);
+    if(file){
+        cvCTRL.classList.add('added')
+        let cv = `<div class="fileView">
+            <p>${file.name}</p>
+            <button onclick="clearFile()">x</button>
+        </div>`
+        
+        cvPrev.innerHTML = cv;
+    }else{
+        cvCTRL.classList.remove('added')
+        cvINP.files = []
+    }
+}
+
+function clearFile(){
+    console.log('clicked');
+    cvCTRL.classList.remove('added')
+    cvINP.value = ""
+}
--- scripts/landing/slider.js
+++ scripts/landing/slider.js
@@ -1,22 +1,30 @@
-let slideSize = document.querySelector('.news .sliderContainer .viewport .slider .slide').clientWidth
+let newsSlides = document.querySelectorAll('.news .sliderContainer .viewport .slider .slide')
+let slideSize = newsSlides[0].clientWidth
 let slider = document.querySelector('.news .sliderContainer .viewport .slider')
 let leftBTN = document.querySelector('.news .sliderContainer button')
+let newsIDX = 3
 let pos = 0;
-let endOfSlider = false
+const activateNews = !(newsSlides.length <= 3)
+let endOfSlider = (newsIDX >= newsSlides.length)
 let sliderWidth = slider.clientWidth
 
 leftBTN.addEventListener('click', ()=>{
-    if(!endOfSlider){
-        pos -= slideSize;
-        slider.style.left = (pos) + "px"
-        endOfSlider = (sliderWidth+pos <= 2 * slideSize)
-    }else{
-        pos += slideSize;
-        slider.style.left = (pos) + "px"
-        endOfSlider = (sliderWidth+pos < sliderWidth)
-    }
+    if(activateNews)
+        if(!endOfSlider){
+            newsIDX++;
+            pos -= slideSize;
+            slider.style.left = (pos) + "px"
+            endOfSlider = (newsIDX >= newsSlides.length)
+        }else{
+            newsIDX--;
+            pos += slideSize;
+            slider.style.left = (pos) + "px"
+            endOfSlider = !(newsIDX <= 3)
+        }
 
-    if(endOfSlider)
+    if(!activateNews)
+        leftBTN.setAttribute('disabled')
+    else if(endOfSlider)
         leftBTN.style.transform = " translateY(-50%) rotate(180deg)"
     else
         leftBTN.style.transform = " translateY(-50%) rotate(0deg)"
